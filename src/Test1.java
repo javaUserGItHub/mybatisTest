3dfc82656ebf5515eb38e99b3c1a2f2ec7deda3a
javaUserGitHub <2929582988@qq.com>
2018-05-01 15:45:41 +0800

    springboot_bootstrap redis  测试

--- src/Test1.java
+++ src/Test1.java
@@ -6,6 +6,7 @@ public class Test1 {
     }
     public boolean equals(Object anObject) {
         //if (this == anObject) {
+        System.out.println();
             return true;
        // }
     }
